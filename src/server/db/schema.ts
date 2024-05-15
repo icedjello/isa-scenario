a67baebebe1b890cc64f473408658f68b48382ba
icedjello <revues.counter0x@icloud.com>
2024-05-15 16:25:47 +0100

    db schema + seed

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -2,27 +2,31 @@
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
 import { sql } from "drizzle-orm";
-import { index, int, sqliteTableCreator, text } from "drizzle-orm/sqlite-core";
+import { int, real, text, sqliteTable } from "drizzle-orm/sqlite-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-export const createTable = sqliteTableCreator((name) => `isa-scenario_${name}`);
+export const products = sqliteTable("products", {
+  id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  name: text("name", { length: 256 }).notNull(),
+  description: text("description", { length: 1000 }).notNull(),
+  ipa: real("ipa").notNull(),
+  createdAt: text("timestamp")
+    .notNull()
+    .default(sql`(current_timestamp)`),
+});
 
-export const posts = createTable(
-  "post",
-  {
-    id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-    name: text("name", { length: 256 }),
-    createdAt: int("created_at", { mode: "timestamp" })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: int("updatedAt", { mode: "timestamp" }),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+export const investments = sqliteTable("investments", {
+  id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  product_id: int("product_id")
+    .notNull()
+    .references(() => products.id),
+  value: real("value").notNull(),
+  currency: text("currency", { enum: ["USD", "GBP", "EUR"] })
+    .notNull()
+    .default("GBP"),
+  status: text("status", { enum: ["Pending", "Completed"] })
+    .notNull()
+    .default("Pending"),
+  createdAt: text("timestamp")
+    .notNull()
+    .default(sql`(current_timestamp)`),
+});
--- src/server/db/seed.ts
+++ src/server/db/seed.ts
@@ -1,5 +1,52 @@
+import { products } from "./schema";
+import { db } from "./index";
+
 const main = async () => {
-  console.log("from seed");
+  const data: (typeof products.$inferInsert)[] = [
+    {
+      name: "The Good ISA",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      ipa: 4.135,
+    },
+    {
+      name: "The Okay ISA",
+      description:
+        "Id aliquet lectus proin nibh nisl. Mi bibendum neque egestas congue quisque egestas diam. Tempor id eu nisl nunc mi ipsum.",
+      ipa: 2.1,
+    },
+    {
+      name: "The Great ISA",
+      description:
+        "A condimentum vitae sapien pellentesque habitant morbi tristique senectus. Gravida arcu ac tortor dignissim convallis.",
+      ipa: 8,
+    },
+    {
+      name: "The Green ISA",
+      description:
+        "Mi eget mauris pharetra et ultrices. Blandit libero volutpat sed cras ornare arcu. Gravida quis blandit turpis cursus in hac habitasse platea.",
+      ipa: 10.22,
+    },
+    {
+      name: "The Evil ISA",
+      description:
+        "Mauris cursus mattis molestie a iaculis. Ut consequat semper viverra nam libero justo laoreet sit. At varius vel pharetra vel turpis nunc eget lorem dolor.",
+      ipa: 6.66,
+    },
+    {
+      name: "The Standard ISA",
+      description:
+        "Gravida cum sociis natoque penatibus et magnis dis. Vitae auctor eu augue ut lectus arcu bibendum at varius. Nulla facilisi nullam vehicula ipsum a arcu.",
+      ipa: 5,
+    },
+    {
+      name: "The Other ISA",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipiscing elit duis. Eu turpis egestas pretium aenean pharetra magna ac placerat.",
+      ipa: 7.89,
+    },
+  ];
+  await db.insert(products).values(data);
 };
 
 void main();
