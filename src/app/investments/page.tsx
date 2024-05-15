5f1fdfd9b269e7c51968ee70ff811940b59cf190
icedjello <revues.counter0x@icloud.com>
2024-05-15 21:37:55 +0100

    page fix

--- src/app/investments/page.tsx
+++ src/app/investments/page.tsx
@@ -1,3 +1,86 @@
-export default function Page() {
-  return <main>hello</main>;
+import Link from "next/link";
+import type { investments, products } from "~/server/db/schema";
+import { type InferSelectModel } from "drizzle-orm";
+import Card from "~/components/Card";
+import { twMerge } from "tailwind-merge";
+
+export default async function Page() {
+  const investmentsRes = await fetch("http://localhost:3000/api/investments", {
+    cache: "no-store",
+  });
+  const investmentsData = (await investmentsRes.json()) as InferSelectModel<
+    typeof investments
+  >[];
+
+  const hasInvestments = investmentsData.length > 0;
+
+  const getProduct = async (product_id: number) => {
+    const productRes = await fetch(
+      `http://localhost:3000/api/products?id=${product_id}`,
+      {
+        cache: "no-store",
+      },
+    );
+    return (await productRes.json()) as InferSelectModel<typeof products>;
+  };
+
+  const investedProducts = hasInvestments
+    ? await Promise.all(
+        investmentsData.map(async ({ product_id, ...rest }) => ({
+          ...rest,
+          product: await getProduct(product_id),
+        })),
+      )
+    : [];
+
+  return (
+    <main>
+      <h1>Your Investments</h1>
+      {hasInvestments ? (
+        <Card outer additionalStyles="grid grid-cols-4">
+          {investedProducts.map(({ id, value, currency, status, product }) => (
+            <Card key={`investment-${id}`}>
+              <Link
+                href={`/products/${product.id}`}
+                className="flex flex-col items-center"
+              >
+                <h2 className="text-companyPink mb-1 text-xl">
+                  {product.name}
+                </h2>
+                <p className="mb-2 font-bold">
+                  Amount: {currency === "GBP" && "£"}
+                  {value}
+                </p>
+                <p
+                  className={twMerge(
+                    status === "Pending" ? "text-amber-400" : "bg-green-400",
+                    "mb-2",
+                  )}
+                >
+                  {status}
+                </p>
+                <p className="mb-2">Interest/Yr: {product.ipa}</p>
+                <p className="mb-2 line-clamp-2">{product.description}</p>
+                <small className="decoration-companyBlue hover:underline">
+                  Click to see more
+                </small>
+              </Link>
+            </Card>
+          ))}
+        </Card>
+      ) : (
+        <Card outer additionalStyles="flex items-center justify-center">
+          <Card additionalStyles="items-center justify-center flex flex-col">
+            <p>You don&apos;t have any investments</p>
+            <Link
+              href="/investments/new-investments"
+              className="decoration-companyBlue text-companyPink text-lg hover:underline"
+            >
+              Click to add investment
+            </Link>
+          </Card>
+        </Card>
+      )}
+    </main>
+  );
 }
